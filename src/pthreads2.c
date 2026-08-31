84847e47cbab93dec199e3d0481e52e7153bf844
Yan Nunes <yrn@cesar.school>
2026-08-31 08:58:07 -0300

    finaliza e integra estrategia Pthreads 2

--- src/pthreads2.c
+++ src/pthreads2.c
@@ -36,3 +36,55 @@ static void *pega_linha(void *arg) {
 
     return NULL;
 }
+
+int faz_pthreads2(unsigned char *image, const config *config) {
+    int n = config->threads;
+
+    if (n > config->altura) {
+        n = config->altura;
+    }
+
+    pthread_t *threads =
+        malloc((size_t) n * sizeof(pthread_t));
+
+    filatrab fila;
+
+    fila.imagem = image;
+    fila.config = config;
+    fila.proxima_linha = 0;
+    fila.erro = 0;
+
+    int criadas = 0;
+
+    for (int i = 0; i < n; i++) {
+        int rc = pthread_create(
+            &threads[i],
+            NULL,
+            pega_linha,
+            &fila
+        );
+
+        if (rc != 0) {
+            fprintf(stderr,
+                    "Erro: falha ao criar thread %d.\n",
+                    i);
+
+            for (int j = 0; j < criadas; j++) {
+                pthread_join(threads[j], NULL);
+            }
+
+            free(threads);
+            return -1;
+        }
+
+        criadas++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    free(threads);
+
+    return 0;
+}   
